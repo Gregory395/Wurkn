fab95be55612c2b4c562628247c624ec61827c44
gregory3francis <gregory3francis@Gregorys-MacBook-Pro.local>
2021-07-23 15:58:36 -0400

    update

--- app.js
+++ app.js
@@ -6,16 +6,43 @@
 
 //https://api.adzuna.com/v1/api/jobs/gb/jobsworth?app_id=&app_key=&title=Javascript%20developer&description=Backbone,HTML5,CSS3&content-type=application/json//
 
+Form.addEventListener('submit', async function (event) {
+    event.preventDefault()
 
-nse: = await fetch (BASE_URL);
-const data = response.json();
-const {description,title,created,display_name,id,tag,redirect_url} = data;
 
-document.getElementById(`description`).textContent = description;
-document.getElementById(`title`).textContent = title;
-document.getElementById(`created`).textContent = created;
-document.getElementById(`display_name`).textContent = display_name;
+const BASE_URL = 'http://api.adzuna.com/v1/api/jobs/gb/jobsworth?app_id=&app_key=&title=Javascript%20developer&description=Backbone,HTML5,CSS3&content-type=application/json';
 
-}
+    async function  getWurkn() {
+        const response = await fetch(BASE_URL);
+        const data = response.json();
+        const {description, title, created, display_name, id, tag, redirect_url} = wurk;
 
-getWurkn();
+        jobResultTag.innerHTML=
+            <h1 class= "text-2xl" >" The kind of Wurk your looking for ${jobTitle} in Massachusetts</h1>
+
+       wurk.forEach(function (job){
+ const div = document.createElement( `div`)
+           div.innerHTML=
+               <p class = ${job.title} </p>
+
+
+
+
+
+
+        }
+
+
+        document.getElementById(`wurk`).textContent = wurk;
+        document.getElementById(`description`).textContent = description;
+        document.getElementById(`title`).textContent = title;
+        document.getElementById(`created`).textContent = created;
+        document.getElementById(`display_name`).textContent = display_name;
+        document.getElementById(`id`).textContent = id;
+        document.getElementById(`tag`).textContent = tag;
+        document.getElementById(`redirect_url`).textContent = redirect_url;
+
+
+        jobResultTag.getWurkn(div);
+    })
+})
